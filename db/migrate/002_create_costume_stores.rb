1fb0622399bb16f0e6a94311450873555d6738f6
Chandon Porter <cneilporter94@gmail.com>
2019-08-20 16:07:07 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,14 @@
-# Create your costume_stores migration here
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+
+def change 
+    create_table :costume_stores do |costume_stores|
+      costume_stores.string :name
+      costume_stores.string :location
+      costume_stores.integer :costume_inventory
+      costume_stores.integer :num_of_employees
+      costume_stores.boolean :still_in_business
+      costume_stores.datetime :opening_time
+      costume_stores.datetime :closing_time
+    end
+  end
+end
--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,1 +1,15 @@
-# Create your haunted_houses migration here
+class CreateHauntedHouses < ActiveRecord::Migration[5.2]
+
+def change 
+    create_table :haunted_houses do |haunted_houses|
+      haunted_houses.string :name
+      haunted_houses.string :location
+      haunted_houses.string :theme
+      haunted_houses.boolean :family_friendly
+      haunted_houses.integer :price
+      haunted_houses.string :opening_date
+      haunted_houses.string :closing_date
+      haunted_houses.string :description
+    end
+  end
+end
